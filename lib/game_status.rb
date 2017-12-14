7ef17455d84adbaf062b3da0b3123360706c8bf8
Valerie Lynie McClimon <vmcclimon33@yahoo.com>
2017-12-14 16:00:51 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,19 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5], # Middle row
+  [6,7,8], # Bottom row
+  [0,3,6], # Left column
+  [1,4,7], # Middle column
+  [2,5,8], # Right column
+  [0,4,8], # Left to right diagonal
+  [2,4,6], # Right to left diagonal
+]
+
+def won?(board)
+  WIN_COMBINATIONS.detect do |i|
+    board[i[0]] == board[i[1]] && board[i[0]] == board[i[2]] && position_taken?(board,i[0])
+  end
+end
